0cc2244442d85ee0e4944ad573747b89f1e5129a
leighkristine <lkroxmob@gmail.com>
2017-09-20 16:41:11 -0700

    added documentation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,26 @@
 
-
-//loop to append buttons
 $(document).ready(function(startup) {
 
+//create topics array
 var topics = ["yarn", "knitting", "video games", "books", "superheros", "makeup"];
 
 
+//function to search GIFPHY, create buttons
     function renderBtn() {
-
+        //empty div
         $("#buttons").empty();
 
-
+        //onclick event 
         $("#add-topic").on("click", function(event) {
             event.preventDefault();
-
+            //add new topic to existing array
             var newTopic = $("#topic-input").val().trim();
             topics.push(newTopic);
             console.log(topics);
             renderBtn();
             $("#topic-input").val("");
-            // reset();
 
+            //start ajax request
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newTopic + "&api_key=e9f698149eb24e8faa00b5a0df504dc9&limit=10";
             
             $.ajax({
@@ -28,11 +28,12 @@ var topics = ["yarn", "knitting", "video games", "books", "superheros", "makeup"
                 method: "GET"
             })
 
+            //response
             .done(function(response) {
 
                 var results = response.data;
                 console.log(results);
-
+                //show gifs per search input   
                 for (var i = 0; i < results.length; i++) {
                     if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
                         var topicImg = $("<img>")
@@ -42,7 +43,7 @@ var topics = ["yarn", "knitting", "video games", "books", "superheros", "makeup"
                 };
             });
         });
-
+        //add buttons to top
         for (var i = 0; i < topics.length; i++) {
             var newBtn = $("<button>");
             newBtn.addClass("topic");
